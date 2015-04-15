3389177a6c0826ad6a9dfbca09acee0d2dce4dfe
shaanxizhuzhe@163.com <shaanxizhuzhe@163.com>
2015-04-15 19:24:13 +0800

    add demo

---
+++ avlib_demo/src/com/snicesoft/avlib/demo/MainActivity.java
@@ -0,0 +1,40 @@
+package com.snicesoft.avlib.demo;
+
+import android.view.View;
+import android.view.View.OnClickListener;
+
+import com.snicesoft.avlib.base.AvActivity;
+import com.snicesoft.avlib.demo.hd.MainHD.MainData;
+import com.snicesoft.avlib.demo.hd.MainHD.MainHolder;
+
+public class MainActivity extends AvActivity<MainHolder, MainData> implements
+		OnClickListener {
+
+	@Override
+	public void createHolderAndData() {
+		holder = new MainHolder();
+		data = new MainData();
+	}
+
+	@Override
+	public void loadContentView() {
+		setContentView(R.layout.activity_main);
+	}
+
+	@Override
+	public void onAv() {
+		dataBind();
+	}
+
+	@Override
+	public void onClick(View v) {
+		switch (v.getId()) {
+		case R.id.btnTest:
+
+			break;
+
+		default:
+			break;
+		}
+	}
+}
---
+++ avlib_demo/src/com/snicesoft/avlib/demo/hd/MainHD.java
@@ -0,0 +1,28 @@
+package com.snicesoft.avlib.demo.hd;
+
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.snicesoft.avlib.annotation.DataBind;
+import com.snicesoft.avlib.annotation.Id;
+import com.snicesoft.avlib.demo.R;
+import com.snicesoft.avlib.widget.IAvData;
+import com.snicesoft.avlib.widget.IAvHolder;
+
+public interface MainHD {
+	public class MainHolder implements IAvHolder {
+		@Id(R.id.tvText)
+		public TextView tvText;
+		@Id(R.id.btnTest)
+		public Button btnTest;
+
+	}
+
+	public class MainData implements IAvData {
+		@DataBind(id = R.id.tvText)
+		public String text = "我是来自MainData";
+		@DataBind(id = R.id.btnTest)
+		public String btn = "From MainData";
+
+	}
+}
