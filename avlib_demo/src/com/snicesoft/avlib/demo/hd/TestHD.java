8b8dd430f205aceb58a68bf385c75044c3531c03
shaanxizhuzhe@163.com <shaanxizhuzhe@163.com>
2015-05-16 13:55:23 +0800

    add

---
+++ avlib_demo/src/com/snicesoft/avlib/demo/hd/TestHD.java
@@ -0,0 +1,12 @@
+package com.snicesoft.avlib.demo.hd;
+
+import com.snicesoft.avlib.rule.IData;
+import com.snicesoft.avlib.rule.IHolder;
+
+public class TestHD {
+	public static class TestHolder implements IHolder {
+	}
+
+	public static class TestData implements IData {
+	}
+}
