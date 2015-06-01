a661d4c7cf18299a5d02f106285bb5e26f8d6836
shaanxizhuzhe@163.com <shaanxizhuzhe@163.com>
2015-06-01 19:11:05 +0800

    update

--- avlib_demo/src/com/snicesoft/avlib/demo/adapter/LVAdapter.java
+++ avlib_demo/src/com/snicesoft/avlib/demo/adapter/LVAdapter.java
@@ -6,24 +6,17 @@ import com.snicesoft.avlib.annotation.Layout;
 import com.snicesoft.avlib.demo.R;
 import com.snicesoft.avlib.demo.adapter.hd.LvHD.LVData;
 import com.snicesoft.avlib.demo.adapter.hd.LvHD.LVHolder;
-import com.snicesoft.avlib.rule.IRule;
 import com.snicesoft.avlib.widget.AvAdapter;
 
 @Layout(R.layout.item_lv)
-public class LVAdapter extends AvAdapter<LVHolder, LVData> implements IRule {
-	private Context context;
+public class LVAdapter extends AvAdapter<LVHolder, LVData> {
 
 	public LVAdapter(Context context) {
-		this.context = context;
+		super(context);
 	}
 
 	@Override
 	public LVHolder newHolder() {
 		return new LVHolder(getContext());
-	}
-
-	@Override
-	public Context getContext() {
-		return context;
 	}
 }
